0580a0e4916425b0653844cf976c739e7baa32a7
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-07-12 16:07:06 -0300

    first commit

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
+
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  ngOnChanges() {}
+
+  ngOnDestroy() {}
+
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+
+  compradoOuNao() {
+    this.item.comprado = !this.item.comprado
+  }
+
+  deletarItem() {
+    this.emitindoIdParaDeletar.emit(this.item.id)
+  }
+
+
+}
